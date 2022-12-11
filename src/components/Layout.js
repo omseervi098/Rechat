5f2c3bbb3a96e79e7708bbcbb38a6fbd58680c23
Omprakash Choudhary <omseervi098@gmail.com>
2022-12-12 03:17:13 +0530

    creating layout

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,10 +1,18 @@
+import React from "react";
+import Left from "./Left";
+import Right from "./Right";
+import Layout from "./Layout";
 function Main() {
   return (
     <div className="App">
       <div className="main container">
         <div className="row">
-          <div className="col-3"></div>
-          <div className="col-9"></div>
+          <div className="col-3">
+            <Layout />
+          </div>
+          <div className="col-9">
+            <Right />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Left.js
@@ -0,0 +1,5 @@
+import React from "react";
+function Left(props) {
+  return <div className="left"></div>;
+}
+export default Left;
